61aa8184752522127fa92352a7f09040a0fcb322
YoanFay <yoanfayolle.yf@gmail.com>
2023-03-23 10:58:51 +0100

    Autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -5,33 +5,45 @@ namespace App;
 class Autoloader
 {
 
-    static function register()
+    /**
+     * Fonction Register de l'Autoloader
+     */
+    public static function register()
     {
-        spl_autoload_register([
-            __CLASS__,
-            'autoload'
-        ]);
+        spl_autoload_register(
+            [
+                __CLASS__,
+                'autoload',
+            ]
+        );
     }
 
-    static function autoload($class)
+
+    /**
+     * Autoload
+     */
+    public static function autoload($class)
     {
-        if (substr($class, 0, 5) === 'Twig\\') {
-            $class = "Vendor\\Twig\\Twig\\Src\\" . substr($class, 5);
-        } elseif (substr($class, 0, 7) === 'Dotenv\\') {
-            $class = "Vendor\\vlucas\\phpdotenv\\Src\\" . substr($class, 7);
-        } elseif (substr($class, 0, 10) === 'PhpOption\\') {
-            $class = "Vendor\\PhpOption\\PhpOption\\Src\\PhpOption\\" . substr($class, 10);
-        } elseif (substr($class, 0, 26) === 'GrahamCampbell\\ResultType\\') {
-            $class = "vendor\\graham-campbell\\result-type\\src\\" . substr($class, 26);
-        } elseif (substr($class, 0, 20) === 'PHPMailer\\PHPMailer\\') {
-            $class = "Vendor\\PHPMailer\\PHPMailer\\Src\\" . substr($class, 20);
+        $vendorPaths = [
+            'Twig\\' => 'Vendor\\Twig\\Twig\\Src\\',
+            'Dotenv\\' => 'Vendor\\vlucas\\phpdotenv\\Src\\',
+            'PhpOption\\' => 'Vendor\\PhpOption\\PhpOption\\Src\\PhpOption\\',
+            'GrahamCampbell\\ResultType\\' => 'vendor\\graham-campbell\\result-type\\src\\',
+            'PHPMailer\\PHPMailer\\' => 'Vendor\\PHPMailer\\PHPMailer\\Src\\',
+        ];
+
+        foreach ($vendorPaths as $namespace => $vendorPath) {
+            if (strpos($class, $namespace) === 0) {
+                $class = $vendorPath . substr($class, strlen($namespace));
+                break;
+            }
         }
+
         $class = str_replace(__NAMESPACE__ . '\\', '', $class);
         $file = __DIR__ . '\\' . $class . '.php';
 
-        if (file_exists($file)) {
-            require_once $file;
+        if (is_file($file)) {
+            include_once $file;
         }
-
     }
 }
