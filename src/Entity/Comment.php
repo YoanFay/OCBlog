58a9385f0137e16cded07da497f72274b058f1fe
YoanFay <yoanfayolle.yf@gmail.com>
2023-04-07 11:39:14 +0200

    Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -47,57 +47,69 @@ class Comment
      */
     private $user_id;
 
+
     /**
-     * @param bool     $init   parameter
-     * @param int|null $idPost parameter
+     * @param bool|string $init   parameter
+     * @param int|null    $idPost parameter
      */
-    public function __construct(bool $init = false, int $idPost = null)
+    public function __construct($init = false, int $idPost = null)
     {
-        if ($init == "default") {
+
+        if ($init === "default") {
             $this->default($idPost);
         }
-    }
+
+    }//end __construct()
+
 
     /**
      * @param int $id parameter
      *
      * @return void
      */
-    public function default(int $id)
+    public function default(int $idPost)
     {
+
         $session = new Session();
         $this->created_at = date_format(new \DateTime(), 'Y-m-d H:i:s');
         $this->updated_at = null;
         $this->deleted_at = null;
-        $this->post_id = $id;
+        $this->post_id = $idPost;
         $this->user_id = $session->getAuth('user_id');
     }
+
 
     /**
      * @return int
      */
     public function getId(): int
     {
+
         return $this->id;
     }
 
+
     /**
-     * @param int $id parameter
+     * @param int $post_id parameter
      *
      * @return void
      */
-    public function setId(int $id): void
+    public function setId(int $post_id): void
     {
-        $this->id = $id;
+
+        $this->id = $post_id;
     }
+
 
     /**
      * @return string
      */
     public function getContent(): string
     {
+
         return $this->content;
     }
+
 
     /**
      * @param string $content parameter
@@ -106,16 +118,20 @@ class Comment
      */
     public function setContent(string $content): void
     {
+
         $this->content = $content;
     }
+
 
     /**
      * @return mixed
      */
     public function getCreatedAt()
     {
+
         return $this->created_at;
     }
+
 
     /**
      * @param mixed $created_at parameter
@@ -124,16 +140,20 @@ class Comment
      */
     public function setCreatedAt($created_at): void
     {
+
         $this->created_at = $created_at;
     }
+
 
     /**
      * @return mixed
      */
     public function getValidatedAt()
     {
+
         return $this->validated_at;
     }
+
 
     /**
      * @param mixed $validated_at parameter
@@ -142,16 +162,20 @@ class Comment
      */
     public function setValidatedAt($validated_at): void
     {
+
         $this->validated_at = $validated_at;
     }
+
 
     /**
      * @return mixed
      */
     public function getUpdatedAt()
     {
+
         return $this->updated_at;
     }
+
 
     /**
      * @param mixed $updated_at parameter
@@ -160,16 +184,20 @@ class Comment
      */
     public function setUpdatedAt($updated_at): void
     {
+
         $this->updated_at = $updated_at;
     }
+
 
     /**
      * @return mixed
      */
     public function getDeletedAt()
     {
+
         return $this->deleted_at;
     }
+
 
     /**
      * @param mixed $deleted_at parameter
@@ -178,16 +206,20 @@ class Comment
      */
     public function setDeletedAt($deleted_at): void
     {
+
         $this->deleted_at = $deleted_at;
     }
+
 
     /**
      * @return int
      */
     public function getPostId(): int
     {
+
         return $this->post_id;
     }
+
 
     /**
      * @param int $post_id parameter
@@ -196,16 +228,20 @@ class Comment
      */
     public function setPostId(int $post_id): void
     {
+
         $this->post_id = $post_id;
     }
+
 
     /**
      * @return int
      */
     public function getUserId(): int
     {
+
         return $this->user_id;
     }
+
 
     /**
      * @param int $user_id parameter
@@ -214,6 +250,7 @@ class Comment
      */
     public function setUserId(int $user_id): void
     {
+
         $this->user_id = $user_id;
     }
 
