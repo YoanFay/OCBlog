a781d331456d05be8d4cbcb001b60799ba10db93
YoanFay <yoanfayolle.yf@gmail.com>
2023-05-09 18:28:16 +0200

    dernier fix

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -52,30 +52,36 @@ class Contact
 
 
     /**
-     * @var bool $init parameter
+     * @var mixed $init parameter
      */
-    public function __construct(bool $init = false)
+    public function __construct($init = false)
     {
+
         if ($init === "default") {
             $this->default();
         }
     }
+
 
     /**
      * @return void
      */
     public function default()
     {
+
         $this->created_at = date_format(new \DateTime(), 'Y-m-d H:i:s');
     }
+
 
     /**
      * @return int
      */
     public function getId(): int
     {
+
         return $this->id;
     }
+
 
     /**
      * @param int $id parameter
@@ -84,16 +90,20 @@ class Contact
      */
     public function setId(int $id): void
     {
+
         $this->id = $id;
     }
+
 
     /**
      * @return string
      */
     public function getName(): string
     {
+
         return $this->name;
     }
+
 
     /**
      * @param string $name parameter
@@ -102,16 +112,20 @@ class Contact
      */
     public function setName(string $name): void
     {
+
         $this->name = $name;
     }
+
 
     /**
      * @return string
      */
     public function getMail(): string
     {
+
         return $this->mail;
     }
+
 
     /**
      * @param string $mail parameter
@@ -120,16 +134,20 @@ class Contact
      */
     public function setMail(string $mail): void
     {
+
         $this->mail = $mail;
     }
+
 
     /**
      * @return string
      */
     public function getMessage(): string
     {
+
         return $this->message;
     }
+
 
     /**
      * @param string $message parameter
@@ -138,16 +156,20 @@ class Contact
      */
     public function setMessage(string $message): void
     {
+
         $this->message = $message;
     }
+
 
     /**
      * @return mixed
      */
     public function getCreatedAt()
     {
+
         return $this->created_at;
     }
+
 
     /**
      * @param mixed $created_at parameter
@@ -156,16 +178,20 @@ class Contact
      */
     public function setCreatedAt($created_at): void
     {
+
         $this->created_at = $created_at;
     }
+
 
     /**
      * @return string|null
      */
     public function getProcess(): ?string
     {
+
         return $this->process;
     }
+
 
     /**
      * @param string|null $process parameter
@@ -174,16 +200,20 @@ class Contact
      */
     public function setProcess(string $process): void
     {
+
         $this->process = $process;
     }
+
 
     /**
      * @return mixed|null
      */
     public function getProcessAt()
     {
+
         return $this->process_at;
     }
+
 
     /**
      * @param mixed|null $process_at parameter
@@ -192,16 +222,20 @@ class Contact
      */
     public function setProcessAt($process_at): void
     {
+
         $this->process_at = $process_at;
     }
+
 
     /**
      * @return int|null
      */
     public function getProcessBy(): ?int
     {
+
         return $this->process_by;
     }
+
 
     /**
      * @param int|null $process_by parameter
@@ -210,16 +244,20 @@ class Contact
      */
     public function setProcessBy(int $process_by): void
     {
+
         $this->process_by = $process_by;
     }
+
 
     /**
      * @return string|null
      */
     public function getAnswer(): ?string
     {
+
         return $this->answer;
     }
+
 
     /**
      * @param string|null $answer parameter
@@ -228,6 +266,7 @@ class Contact
      */
     public function setAnswer(?string $answer): void
     {
+
         $this->answer = $answer;
     }
 
